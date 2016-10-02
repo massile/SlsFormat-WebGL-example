66cbd0c542da6a9231e7af11735e9e4765c4710b
Massinissa Mokhtari <mokhtari.massinissa@gmail.com>
2016-10-02 23:32:51 +0200

    Get webpack ready

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var Webpack = require('webpack');
+
+module.exports = {
+	entry: __dirname + '/src/index.js',
+	output: {
+		path: __dirname + '/build',
+		filename: 'bundle.js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "WebGL hot reloader",
+			template: __dirname + '/src/index.html'
+		}),
+		new Webpack.DefinePlugin({
+			__DEV__: process.env.NODE_ENV !== 'production'
+		})
+	],
+	resolve: {
+		root: [__dirname + '/src']
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				include: __dirname + '/src',
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					cacheDirectory: true,
+					presets: ['latest']
+				}
+			},
+			{
+				test: /\.glsl$/,
+				include: '/resources/shaders',
+				loader: 'webpack-glsl'
+			}
+		]
+	},
+	debug: process.env.NODE_ENV !== 'production',
+	devtool: 'eval-source-map'
+}
